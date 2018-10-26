4692ad9781b6199783d586ce8e9b53aa101f638e
o84118317 <o84118317@gmail.com>
2018-10-26 16:37:19 +0800

    Leveraging Constants

--- 01_Creating_Store.js
+++ 01_Creating_Store.js
@@ -1,90 +1,4 @@
-{
-	type: 'ADD_TODO',
-	todo: {
-		id: 0,
-		name: 'Learn Redux',
-		complete: false,
-	}
-}
-
-{
-	type: 'Remove_TODO',
-	id: 0,
-}
-
-{
-	type: 'TOGGLE_TODO',
-	id: 0,
-}
-
-{
-	type: 'ADD_GOAL',
-	goal: {
-		id: 0,
-		name: 'Run a Marathon'
-	}
-}
-
-{
-	type: 'REMOVE_GOAL',
-	id: 0
-}
-
-
-
-/*
-Pure function:
-1. They always return the same result if the same arguments are passed in
-2. They depend only on the arguments passed into them.
-3. Never produce any side effects.
-*/
-
-// Reducer function
-
-function todos (state = [], action) {
-	switch (action.type) {
-		case 'ADD_TODO':
-			return state.concat([action.todo])
-		case 'Remove_TODO':
-			return state.filter((todo) => todo.id !== action.id)
-		case 'TOGGLE_TODO':
-			return state.map((todo) => todo.id !== action.id ? todo : 
-				// name: todo.name,
-				// id: todo.id,
-				// complete: !todo.complete
-				Object.assign({}, todo, {complete: !todo.complete})
-			)
-		default:
-			return state 
-	}	
-}
-
-
-function goals (state = [], action) {
-	switch(action.type) {
-		case 'ADD_GOAL':
-			return state.concat([action.goal])
-		case 'REMOVE_GOAL':
-			return state.filter((goal) => goal.id !== action.id)
-		default:
-			return state
-	}
-}
-
-
-function app (state = {} , action) {
-	return {
-		todos: todos(state.todos, action),
-		goals: goals(state.goals, action),
-	} 
-}
-
-// {
-// 	todos: [],
-// 	goals: [],
-// }
-
-
+//  Library code
 function createStore (reducer) {
 	/*
 	The store should have four parts
@@ -125,8 +39,103 @@ function createStore (reducer) {
 }
 
 
+
+//  App code
+
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'Remove_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+/*
+Pure function:
+1. They always return the same result if the same arguments are passed in
+2. They depend only on the arguments passed into them.
+3. Never produce any side effects.
+*/
+
+// Reducer function
+
+function todos (state = [], action) {
+	switch (action.type) {
+		case ADD_TODO:
+			return state.concat([action.todo])
+		case REMOVE_TODO:
+			return state.filter((todo) => todo.id !== action.id)
+		case TOGGLE_TODO:
+			return state.map((todo) => todo.id !== action.id ? todo : 
+				// name: todo.name,
+				// id: todo.id,
+				// complete: !todo.complete
+				Object.assign({}, todo, {complete: !todo.complete})
+			)
+		default:
+			return state 
+	}	
+}
+
+
+function goals (state = [], action) {
+	switch(action.type) {
+		case ADD_GOAL:
+			return state.concat([action.goal])
+		case REMOVE_GOAL:
+			return state.filter((goal) => goal.id !== action.id)
+		default:
+			return state
+	}
+}
+
+
+function app (state = {} , action) {
+	return {
+		todos: todos(state.todos, action),
+		goals: goals(state.goals, action),
+	} 
+}
+
+// {
+// 	todos: [],
+// 	goals: [],
+// }
+
+
 const store = createStore(app)
 
 store.subscribe(() => {
 	console.log('The new state is: ', store.getState())
 })
+
+store.dispatch({
+	type: ADD_TODO,
+	todo: {
+		id: 0,
+		name: 'Learn Redux',
+		complete: false,
+	}
+})
+
+store.dispatch({
+	type: REMOVE_TODO,
+	id: 0,
+})
+
+store.dispatch({
+	type: TOGGLE_TODO,
+	id: 0,
+})
+
+store.dispatch({
+	type: ADD_GOAL,
+	goal: {
+		id: 0,
+		name: 'Run a Marathon'
+	}
+})
+
+store.dispatch({
+	type: REMOVE_GOAL,
+	id: 0
+})
+
